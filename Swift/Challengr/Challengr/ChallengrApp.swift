c69ccf4ff2041ec2da4f41af0c414f71061e1662
“Julian <julian.paul.richter@gmail.com>
2025-10-15 09:03:50 +0200

    Swift Projekt erstellt

---
+++ Swift/Challengr/Challengr/ChallengrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengrApp.swift
+//  Challengr
+//
+//  Created by Julian Richter on 15.10.25.
+//
+
+import SwiftUI
+
+@main
+struct ChallengrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift/Challengr/Challengr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challengr
+//
+//  Created by Julian Richter on 15.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
