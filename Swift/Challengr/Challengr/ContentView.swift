c30ffad103e4ea60d100ed5a1b674fbe8aa30edb
“Julian <julian.paul.richter@gmail.com>
2025-10-15 09:29:07 +0200

    Kategorien in Swift Projekt sichtbar

--- Swift/Challengr/Challengr/ContentView.swift
+++ Swift/Challengr/Challengr/ContentView.swift
@@ -8,16 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    let items = [
+            CardItem(
+                image: Image(systemName: "sportscourt"),
+                title: "Fitness",
+                subtitle: "Verschiedene sportliche Challanges!"
+            ),
+            CardItem(
+                image: Image(systemName: "flame"),
+                title: "Mutprobe",
+                subtitle: "Wer traut sich mehr?"
+
+            ),
+            CardItem(
+                image: Image(systemName: "lightbulb"),
+                title: "Wissen",
+                subtitle: "Teste dein Wissen!"
+            ),
+            CardItem(
+                image: Image(systemName: "magnifyingglass"),
+                title: "Suchen",
+                subtitle: "Wer findet etwas zuerst?"
+            )
+        ]
+
+        var body: some View {
+
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(items) { item in
+                        CardView(item: item)
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.vertical, 20)
+            }
+            .background(Color(UIColor.systemGray6))
+            .edgesIgnoringSafeArea(.bottom)
         }
-        .padding()
     }
-}
+
+    struct CardView: View {
+
+        let item: CardItem
+
+        var body: some View {
+            VStack(alignment: .leading, spacing: 12) {
+                HStack {
+                    item.image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(.blue)
+                        .padding(.bottom, 8)
+
+                    Spacer()
+                }
+               
+                Text(item.title)
+                    .font(.headline)
+                    .fontWeight(.bold)
+
+                Text(item.subtitle)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 8)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(color: Color.gray.opacity(0.4), radius: 4, x: 0, y: 2)
+        }
+    }
+
+    struct CardItem: Identifiable {
+        let id = UUID()
+        let image: Image
+        let title: String
+        let subtitle: String
+    }
+
 
 #Preview {
     ContentView()
