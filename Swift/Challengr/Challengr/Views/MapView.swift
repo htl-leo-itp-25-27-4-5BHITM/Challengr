a58354452ce3d036a55a05d61bf1738f57727d4a
Binder Dominik <dominik.binder06@gmx.at>
2025-11-05 10:51:53 +0100

    #17 Swift Mapeinbindung

--- Swift/Challengr/Challengr/Views/MapView.swift
+++ Swift/Challengr/Challengr/Views/MapView.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
 
 struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 0, longitude: 0), // Startwert
+        span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5) // Zoom-Level 5
+    )
+    
+    @State private var locationManager = CLLocationManager()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region, showsUserLocation: true)
+            .onAppear {
+                locationManager.requestWhenInUseAuthorization()
+                locationManager.startUpdatingLocation()
+                
+                if let location = locationManager.location?.coordinate {
+                    region.center = location
+                    region.span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
+                }
+            }
+            .mapControls {
+                MapUserLocationButton() // Zurück zum eigenen Standort
+                MapCompass()
+            }
+            .ignoresSafeArea()
     }
 }
 
