92ae507d8233008bcf9ff80570ba94ee519c4ccb
Sebastian Lehner <basti2006@gmx.at>
2025-11-24 10:43:32 +0100

    post und get routen db

---
+++ Backend/challengrbackend/src/main/java/sql/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO player (name, longitude, latitude) VALUES
+                                                   ('EigenerSpieler', 14.251389, 48.268333),  -- ca. HTL Leonding Standort
+                                                   ('ZweiterSpieler', 14.255000, 48.270000); -- ein Spieler in der Nähe von HTL Leonding
