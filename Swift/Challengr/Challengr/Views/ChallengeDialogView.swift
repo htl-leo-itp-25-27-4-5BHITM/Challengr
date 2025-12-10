0b0f3d688d24bc1696579cddab13493382e96598
Binder Dominik <dominik.binder06@gmx.at>
2025-12-10 09:19:54 +0100

    #57 Entwicklung eines Dialog-Fensters

---
+++ Swift/Challengr/Challengr/Views/ChallengeDialogView.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeDialogView.swift
+//  Challengr
+//
+//  Created by Dominik Binder on 10.12.25.
+//
+
+import SwiftUI
+
+struct ChallengeDialogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChallengeDialogView()
+}
